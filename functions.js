2625090b37ea76e545c1e9799491fb7f51722385
Galvantier <kencole778@gmail.com>
2025-12-19 20:36:57 +0300

    third comit

---
+++ functions.js
@@ -0,0 +1,26 @@
+// Function 1: calculateTax
+function calculateTax(amount) {
+    return amount * 0.10;
+}
+
+// Function 2: convertToUpperCase
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
+
+// Function 3: findMaximum
+function findMaximum(num1, num2) {
+    return Math.max(num1, num2);
+}
+
+// Function 4: isPalindrome
+function isPalindrome(word) {
+    const normalized = word.toLowerCase();
+    const reversed = normalized.split('').reverse().join('');
+    return normalized === reversed;
+}
+
+// Function 5: calculateDiscountedPrice
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    return originalPrice - (originalPrice * (discountPercentage / 100));
+}
